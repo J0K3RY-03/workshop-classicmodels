701d9ab9f7e33b18f2162269e2a655062d3ff91a
J0K3RY <jokery03@hotmail.com>
2023-08-09 15:43:11 +0200

    classicmodel before login

--- src/public/index.php
+++ src/public/index.php
@@ -1,4 +1,20 @@
 <?php
 declare(strict_types=1);
 
-echo 'It works !';
+session_start();
+
+try {
+    // 1 - Connexion à la DB
+    require_once 'public/db/connection.php';
+
+    // 2 - Requête SQL pour récupérer la liste des produits
+    $stmt = $pdo->query("SELECT * FROM products LIMIT 20");
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // 3 - Affichage de la liste des produits
+    include 'public/views/layout/header.view.php';
+    include 'public/views/index.view.php';
+    include 'public/views/layout/footer.view.php';
+} catch (Exception $e) {
+    print_r($e->getMessage());
+}
